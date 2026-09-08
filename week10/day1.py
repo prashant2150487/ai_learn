5fdffbafdd76237ad8e0ba7b7446c6f82bdf2604
Prashant Sachan <Prashant.sachan@isonxperiences.com>
2026-09-08 15:44:32 +0530

    feat: add .gitignore and update day1.py for dataset loading and visualization

--- week10/day1.py
+++ week10/day1.py
@@ -1,9 +1,30 @@
-from tensorflow.keras.datasets import mnist, cifar10
+from tensorflow.keras.datasets import mnist , cifar10
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import torch
+import torch.nn as nn
 
-# load mnist
-(X_train_mnist, y_train_mnist), (X_test_mnist, y_test_mnist) = mnist.load_data()
-print("MNIST dataset loaded successfully", "Training samples:", X_train_mnist.shape, "Testing samples:", X_test_mnist.shape)
+(X_train_mnist, y_train_mnist), (X_test_mnist, y_test_mnist) = mnist.load_data();
+print("Training data shape:", X_train_mnist.shape, X_test_mnist.shape)
+(X_train_cifar, y_train_cifar), (X_test_cifar, y_test_cifar) = cifar10.load_data();
+print("Training data shape:", X_train_cifar.shape, X_test_cifar.shape)
 
-# load cifar10
-(X_train_cifar10, y_train_cifar10), (X_test_cifar10, y_test_cifar10) = cifar10.load_data()
-print("CIFAR-10 dataset loaded successfully", "Training samples:", X_train_cifar10.shape, "Testing samples:", X_test_cifar10.shape)
+# Define basic Dense layer
+layer = tf.keras.layers.Dense(
+    units=10,
+    activation="relu"
+)
+
+print("Layer created successfully:", layer)
+
+# Visualize MNIST example
+plt.imshow(X_train_mnist[0], cmap='gray')
+plt.title(f"MNIST Label: {y_train_mnist[0]}")
+plt.show()
+
+# visualize CIFAR-10 example
+plt.imshow(X_train_cifar[0])
+plt.title(f"CIFAR-10 Label: {y_train_cifar[0]}")
+plt.show()
+
+
